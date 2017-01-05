8636ecfcd6df43c633d7b0b2aa9b6ea6c507748b
Kaloyan Spiridonov <kal0ian7@abv.bg>
2017-01-05 22:41:44 +0200

    initial commit

---
+++ src/main/java/bg/uni/sofia/fmi/number/recognition/entry/ImageEntry.java
@@ -0,0 +1,9 @@
+package bg.uni.sofia.fmi.number.recognition.entry;
+
+import java.util.ArrayList;
+
+public class ImageEntry extends ArrayList<Pixel>{
+
+	private static final long serialVersionUID = 1L;
+
+}
