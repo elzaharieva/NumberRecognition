4b387281759a7e0c6a08072e8a88e18a4c15a547
Kaloyan Spiridonov <kaloyan.spiridonov@sap.com>
2017-02-15 18:38:21 +0200

    webapp initials

--- src/main/java/bg/uni/sofia/fmi/number/recognition/servlet/UploadImageServlet.java
+++ src/main/java/bg/uni/sofia/fmi/number/recognition/servlet/UploadImageServlet.java
@@ -1,7 +1,25 @@
-//package bg.uni.sofia.fmi.number.recognition.servlet;
-//
-//@WebServlet("/upload")
-//@MultipartConfig
-//public class UploadImageServlet extends HttpServlet{
-//
-//}
+package bg.uni.sofia.fmi.number.recognition.servlet;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+@WebServlet("/upload")
+@MultipartConfig
+public class UploadImageServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	    Part filePart = req.getPart("file"); 
+	    String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+	    resp.setContentType("text/plain");
+	    resp.getWriter().write("Hello - "+fileName);
+	    resp.getWriter().close();
+	}
+}
